610144f048ce8a04c3a763b921bd2ca6deff84ac
Brandes Moore <Brandes.Moore20@gmail.com>
2020-01-18 13:53:22 -0600

    Question 2 completed

--- Scripts/data_analyst_jobs.sql
+++ Scripts/data_analyst_jobs.sql
@@ -5,6 +5,13 @@
 SELECT *
 FROM data_analyst_jobs
 
+/*
+2.	Write a query to look at just the first 10 rows. 
+What company is associated with the job posting on the 10th row?
+*/
+SELECT *
+FROM data_analyst_jobs
+LIMIT 10
 
 
 
