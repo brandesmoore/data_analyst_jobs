b8bd67556edb3fbaf5549b6eb7f50a03a387408e
Brandes Moore <Brandes.Moore20@gmail.com>
2020-01-18 15:19:01 -0600

    Question 3 is complete.

--- Scripts/data_analyst_jobs.sql
+++ Scripts/data_analyst_jobs.sql
@@ -23,6 +23,12 @@ SELECT COUNT(*)
 FROM data_analyst_jobs
 WHERE location IN ('TN', 'KY')
 
+/*4.	How many postings in Tennessee have a star rating above 4?
+*/
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE star_rating > 4
+AND location = ‘TN’
 
 
 
