31a6f84c67155b7e378492f65e744587b32494a7
Brandes Moore <Brandes.Moore20@gmail.com>
2020-01-18 15:20:26 -0600

    Question 4 is complete.

--- Scripts/data_analyst_jobs.sql
+++ Scripts/data_analyst_jobs.sql
@@ -30,6 +30,12 @@ FROM data_analyst_jobs
 WHERE star_rating > 4
 AND location = ‘TN’
 
+/*5.	How many postings in the dataset have a review count between 500 and 1000?
+*/
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE review_count BETWEEN 500 AND 1000
+
 
 
 
