f98d56c1d4b61daa779934398b2ef45a48385c22
Brandes Moore <Brandes.Moore20@gmail.com>
2020-01-18 15:41:55 -0600

    Question 6 is complete.

--- Scripts/data_analyst_jobs.sql
+++ Scripts/data_analyst_jobs.sql
@@ -36,6 +36,16 @@ SELECT COUNT(title)
 FROM data_analyst_jobs
 WHERE review_count BETWEEN 500 AND 1000
 
+/*6.	Show the average star rating for each state. 
+The output should show the state as state and the average rating 
+for the state as avg_rating. Which state shows the highest average rating?*/
+SELECT location AS state, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
+	AND location NOT LIKE 'USA'
+	AND location NOT LIKE 'REMOTE'
+GROUP BY location
+ORDER BY avg_rating DESC
 
 
 
