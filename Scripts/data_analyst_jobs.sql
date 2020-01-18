4c68d3e12979126f6b5759afe3ff340e81685c78
Brandes Moore <Brandes.Moore20@gmail.com>
2020-01-18 15:46:52 -0600

    Question 9 is complete.

--- Scripts/data_analyst_jobs.sql
+++ Scripts/data_analyst_jobs.sql
@@ -58,3 +58,13 @@ SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
 WHERE location = 'CA'
 
+/*9.	Find the name of each company and its average star rating 
+for all companies that have more than 5000 reviews across all locations. 
+How many companies are there with more than 5000 reviews across all locations?
+*/
+SELECT company, AVG(star_rating), review_count
+FROM data_analyst_jobs
+WHERE star_rating IS NOT NULL
+GROUP BY company, star_rating, review_count
+HAVING (review_count) > 5000
+
